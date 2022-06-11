4fcd16a44a829dcd28de01d3b5b48b67ed9b554a
Kari Monetti <karinamonetti.dev@gmail.com>
2022-06-11 20:47:27 -0300

    añadiendo ejercicio de condicional con operador ternario

---
+++ ejercicio9.js
@@ -0,0 +1,8 @@
+// CONDICIONAL CON OPERADOR TERNARIO
+// Verificar si el usuario es mayor o menor de edad
+// 1. Guardar un valor numérico ingresado por el usuario
+// 2. Si el valor es mayor a 18 mostrar un mensaje, si es menor visualizar otro.
+
+let age = parseInt(prompt("Dime tu edad: "))
+let operadorTernario= (age>=18)?alert("Eres mayor de edad"):alert("Eres menor de edad");
+operadorTernario;
