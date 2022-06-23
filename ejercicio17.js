26355599078f1ab580d8b7708a37d4211563b433
Kari Monetti <karinamonetti.dev@gmail.com>
2022-06-23 12:16:05 -0300

    EJERCICIO 3, JON MIRCHA: Función que transfroma un string a un array dado cierto caracter

--- ejercicio17.js
+++ ejercicio17.js
@@ -19,5 +19,12 @@ const stringRecortado = () => {
 stringRecortado();
 
 //3) Programa una función que dada una String te devuelva un Array de textos separados por cierto caracter, pe. miFuncion('hola que tal', ' ') devolverá ['hola', 'que', 'tal'].
+const stringEnUnArray = () =>{
+    let dimeAlgunaCosa= prompt("Dime algo y yo lo convertiré en un Array");
+    let dimeAlgunaCosaEnArray= dimeAlgunaCosa.split(" ");
+    return dimeAlgunaCosaEnArray;
+}
+stringEnUnArray();
+
 
 //4) Programa una función que repita un texto X veces, pe. miFuncion('Hola Mundo', 3) devolverá Hola Mundo Hola Mundo Hola Mundo.
