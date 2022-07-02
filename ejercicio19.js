92e4fd86a31259b038c06de3c798fda73b832c1a
Kari Monetti <karinamonetti.dev@gmail.com>
2022-07-02 11:41:52 -0300

    función número capicua

--- ejercicio19.js
+++ ejercicio19.js
@@ -11,5 +11,26 @@ aleatorio();
 
 //10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 
+const capicua = num => {
+    if (typeof num !== "number") {
+        console.log(typeof num)
+        return alert("Debes ingresar un número")
+    } 
+
+    let myString = String(num);
+    let myNewString=myString.split("").reverse().join("")
+    
+    if (myString === myNewString){
+        alert("Es un número capicua")
+    }else{
+        alert("No es capicua")
+    }
+}
+capicua("hola")
+capicua(5556);
+capicua("NaN")
+capicua(NaN);
+capicua(333);
+
 
 //11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
